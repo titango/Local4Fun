7821fbd70153ee3995e799ab057e3f48e8e8fde2
Feng Li <wxzcon@gmail.com>
2019-02-24 18:50:49 -0800

    add user register and login email: wxzcon@gmail.com pwd: 123456

--- app/src/main/java/com/example/tanthinh/local4fun/MainActivity.java
+++ app/src/main/java/com/example/tanthinh/local4fun/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.tanthinh.local4fun;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth mAuth;
+    public String TAG = "MainActivity";
+    EditText loginEmail, loginPwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView registerTV = (TextView) findViewById(R.id.registerTextView);
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginPwd = (EditText) findViewById(R.id.loginPwd);
+        registerTV.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(MainActivity.this, "login success.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+        switch (i){
+            case R.id.registerTextView:
+                Intent it = new Intent(MainActivity.this, acountRegister.class);
+                startActivity(it);
+                break;
+            case R.id.loginBtn:
+                signIn(loginEmail.getText().toString(), loginPwd.getText().toString());
+            default:
+                break;
+        }
     }
 }
