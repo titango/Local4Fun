5f9e70e803e70299873dc649fa040341a4875e24
Feng Li <wxzcon@gmail.com>
2019-02-24 22:45:43 -0800

    password reset

--- app/src/main/java/com/example/tanthinh/local4fun/MainActivity.java
+++ app/src/main/java/com/example/tanthinh/local4fun/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.tanthinh.local4fun;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +21,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static android.widget.ListPopupWindow.MATCH_PARENT;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
@@ -27,17 +35,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         TextView registerTV = (TextView) findViewById(R.id.registerTextView);
+        TextView forgotTV = (TextView) findViewById(R.id.forgotTextView);
         Button loginBtn = (Button) findViewById(R.id.loginBtn);
         loginEmail = (EditText) findViewById(R.id.loginEmail);
         loginPwd = (EditText) findViewById(R.id.loginPwd);
         registerTV.setOnClickListener(this);
         loginBtn.setOnClickListener(this);
+        forgotTV.setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
 
     }
     private void signIn(String email, String password) {
         Log.d(TAG, "signIn:" + email);
-
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -56,6 +65,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 });
     }
+
+    public void setDialog(){
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        final EditText input = new EditText(MainActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        dialog.setMessage("Please enter your email address so we can send you the reset password link.");
+        dialog.setView(input);
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Send email", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                sendEmail(input.getText().toString());
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alert = dialog.create();
+        alert.setTitle("Forgot Password?");
+        alert.show();
+    }
+
+    public void sendEmail(String email){
+        mAuth.sendPasswordResetEmail(email);
+        Toast.makeText(MainActivity.this, "Email sent success.",
+                Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onClick(View view) {
         int i = view.getId();
@@ -66,8 +109,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.loginBtn:
                 signIn(loginEmail.getText().toString(), loginPwd.getText().toString());
+            case R.id.forgotTextView:
+                setDialog();
             default:
                 break;
         }
     }
+
+
+
 }
