0472f53e9593a2baf6db701fa4d57ac431d1b613
Phi Hung Cao <caophihung8392@gmail.com>
2019-02-28 20:38:40 -0800

    uodate

--- app/src/main/java/com/example/tanthinh/local4fun/screens/MainActivity.java
+++ app/src/main/java/com/example/tanthinh/local4fun/screens/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        
+
         toolbar.setTitle("ExploreScreen");
         loadFragment(new ExploreScreenFragment());
     }
