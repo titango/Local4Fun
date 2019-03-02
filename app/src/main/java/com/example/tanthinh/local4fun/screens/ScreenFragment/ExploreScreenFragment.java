070a713523e21185935253f3e3b88048b8efe879
Domingues Rocha de Oliveira <dominguesrochadasilveiras@student.douglascollege.ca>
2019-03-02 15:30:15 -0800

    Added communication with Firebase DB

--- app/src/main/java/com/example/tanthinh/local4fun/screens/ScreenFragment/ExploreScreenFragment.java
+++ app/src/main/java/com/example/tanthinh/local4fun/screens/ScreenFragment/ExploreScreenFragment.java
@@ -3,20 +3,32 @@ package com.example.tanthinh.local4fun.screens.ScreenFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import com.example.tanthinh.local4fun.R;
 import com.example.tanthinh.local4fun.adapters.PostAdapter;
-import com.example.tanthinh.local4fun.adapters.ViewPagerAdapter;
-import com.example.tanthinh.local4fun.screens.MainActivity;
+
+import com.example.tanthinh.local4fun.models.Post;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,6 +46,7 @@ public class ExploreScreenFragment extends Fragment {
     private RecyclerView.LayoutManager layoutManager;
     private Context context;
     private List postList;
+    ArrayList<Post> posts = new ArrayList<Post>();
 
 //    private OnFragmentInteractionListener mListener;
 
@@ -87,6 +100,9 @@ public class ExploreScreenFragment extends Fragment {
 //            throw new RuntimeException(context.toString()
 //                    + " must implement OnFragmentInteractionListener");
 //        }
+
+        getPosts();
+
     }
 
     @Override
@@ -95,6 +111,48 @@ public class ExploreScreenFragment extends Fragment {
 //        mListener = null;
     }
 
+    public void getPosts(){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+
+        Query phoneQuery = myRef.child("posts");
+        phoneQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                    Post p = new Post(singleSnapshot.child("userId").getValue().toString(),
+                            singleSnapshot.child("title").getValue().toString(),
+                            singleSnapshot.child("description").getValue().toString(),
+                            Double.parseDouble(singleSnapshot.child("hours").getValue().toString()),
+                            Double.parseDouble(singleSnapshot.child("pricePerPerson").getValue().toString()));
+
+                    for(DataSnapshot picSnapshot : singleSnapshot.child("pictures").getChildren()){
+                        p.addPicture(picSnapshot.getValue().toString());
+                    }
+                    posts.add(p);
+                }
+
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+
+    }
+
+    private void getPictures(){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+        // Create a reference with an initial file path and name
+        StorageReference pathReference = storageRef.child("aacom/stars.jpg");
+    }
 
 //    public interface OnFragmentInteractionListener {
 //        void onFragmentInteraction(Uri uri);
