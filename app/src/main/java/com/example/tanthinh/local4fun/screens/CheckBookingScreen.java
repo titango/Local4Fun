8f78643e9604ad33b961472f8ce3b17a444f431e
Samuel <samueldroca@gmail.com>
2019-04-03 19:57:15 -0700

    Merging PostDetailsScreen from Tan

---
+++ app/src/main/java/com/example/tanthinh/local4fun/screens/CheckBookingScreen.java
@@ -0,0 +1,124 @@
+package com.example.tanthinh.local4fun.screens;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.tanthinh.local4fun.R;
+import com.example.tanthinh.local4fun.models.Post;
+import com.google.gson.Gson;
+
+import java.util.Calendar;
+
+public class CheckBookingScreen extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private ImageButton back_arrow_btn;
+
+    private DatePickerDialog picker;
+    private EditText eText;
+    private Spinner numPerson;
+    private Spinner numTime;
+    private TextView totalPriceTxtView;
+
+    private double totalPrice = 0;
+    private String numberPerson = "";
+    private Post currentPost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_booking_screen);
+
+        init();
+    }
+
+    private void init()
+    {
+        Intent initIntent = getIntent();
+        final String postString = (String)initIntent.getExtras().get("postObject");
+        Gson gson = new Gson();
+        currentPost = gson.fromJson(postString, Post.class);
+
+        back_arrow_btn = findViewById(R.id.back_arrow_btn);
+        back_arrow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(CheckBookingScreen.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText((monthOfYear + 1) + "/" + dayOfMonth+ "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        numTime = (Spinner)findViewById(R.id.time_spinner);
+        numPerson = (Spinner)findViewById(R.id.num_person_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.num_person, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numPerson.setAdapter(adapter);
+        numPerson.setOnItemSelectedListener(this);
+        numPerson.setSelection(0);
+
+        ArrayAdapter<CharSequence> adapterTime = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        adapterTime.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        numTime.setAdapter(adapterTime);
+        numTime.setOnItemSelectedListener(this);
+        numTime.setSelection(0);
+
+        totalPriceTxtView = (TextView)findViewById(R.id.total_value);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if(adapterView.getId() == R.id.num_person_spinner)
+        {
+            Log.w("selected", "numperson");
+            int numPerson = Integer.parseInt((String)adapterView.getItemAtPosition(i));
+            totalPrice = numPerson * currentPost.getPricePerPerson();
+            totalPriceTxtView.setText("$" + String.valueOf(totalPrice));
+        }else
+        {
+            Log.w("selected", "numTime");
+            numberPerson = (String)adapterView.getItemAtPosition(i);
+            Log.w("num time selected",numberPerson);
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
--- app/src/main/java/com/example/tanthinh/local4fun/screens/PostDetailsScreen.java
+++ app/src/main/java/com/example/tanthinh/local4fun/screens/PostDetailsScreen.java
@@ -1,12 +1,19 @@
 package com.example.tanthinh.local4fun.screens;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.tanthinh.local4fun.R;
@@ -22,6 +29,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+
 import me.relex.circleindicator.CircleIndicator;
 
 public class PostDetailsScreen extends AppCompatActivity implements OnMapReadyCallback {
@@ -36,12 +45,18 @@ public class PostDetailsScreen extends AppCompatActivity implements OnMapReadyCa
     public ViewPager viewPager; // for slider
     private CircleIndicator indicator;
     private ImageButton back_arrow_btn;
+    private Button book_now_btn;
 
     public TextView postTitle;
     public TextView postHour;
     public TextView postTour;
     public TextView postPrice;
     public TextView postLocation;
+    public Post currentPost;
+
+    private LinearLayout locationMarkerWrapper;
+    private LinearLayout localtionLabelWrapper;
+    private LinearLayout reviewsWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +70,9 @@ public class PostDetailsScreen extends AppCompatActivity implements OnMapReadyCa
     {
         //Get post object
         Intent initIntent = getIntent();
-        String postString = (String)initIntent.getExtras().get("postObject");
+        final String postString = (String)initIntent.getExtras().get("postObject");
         Gson gson = new Gson();
-        Post curPost = gson.fromJson(postString, Post.class);
+        currentPost = gson.fromJson(postString, Post.class);
 //        Log.w("CurPost", curPost.getUserId());
 
         ll = initIntent.getParcelableExtra("latLon");
@@ -75,19 +90,20 @@ public class PostDetailsScreen extends AppCompatActivity implements OnMapReadyCa
         postPrice = findViewById(R.id.explore_price_person);
         postLocation = findViewById(R.id.explore_location);
         back_arrow_btn = findViewById(R.id.back_arrow_btn);
+        book_now_btn = findViewById(R.id.book_now_btn);
 
-        viewPagerAdapter = new ViewPagerAdapter(getApplication(), curPost.getPictures());
+        viewPagerAdapter = new ViewPagerAdapter(getApplication(), currentPost.getPictures());
         viewPager.setAdapter(viewPagerAdapter);
 
         indicator = (CircleIndicator) findViewById(R.id.viewPagerIndicator);
         indicator.setViewPager(viewPager);
         viewPagerAdapter.registerDataSetObserver(indicator.getDataSetObserver());
 
-        postTitle.setText(curPost.getTitle());
-        postHour.setText(curPost.getHours() + "");
-        postTour.setText(curPost.getTourType() + "");
-        postPrice.setText(curPost.getPricePerPerson() + "");
-        postLocation.setText(curPost.getLocation() + "");
+        postTitle.setText(currentPost.getTitle());
+        postHour.setText(currentPost.getHours() + "");
+        postTour.setText(currentPost.getTourType() + "");
+        postPrice.setText(currentPost.getPricePerPerson() + "");
+        postLocation.setText(currentPost.getLocation() + "");
 
         back_arrow_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,6 +111,225 @@ public class PostDetailsScreen extends AppCompatActivity implements OnMapReadyCa
                 finish();
             }
         });
+        book_now_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent payIntent = new Intent(getApplicationContext(), CheckBookingScreen.class);
+                payIntent.putExtra("postObject", postString);
+                startActivity(payIntent);
+            }
+        });
+
+        displayPlanSection();
+        displayReviewSection();
+    }
+
+    private void displayPlanSection()
+    {
+        locationMarkerWrapper = (LinearLayout)findViewById(R.id.location_marker_wrapper);
+        localtionLabelWrapper = (LinearLayout)findViewById(R.id.location_label_wrapper);
+
+        // FAKE DATA NOW
+        currentPost.plan = new ArrayList<>();
+        currentPost.plan.add("New West Station && We will meet at new west station first");
+        currentPost.plan.add("Metrotown && Check out shopping at Metrotown");
+        currentPost.plan.add("Downtown && Center of Vancouver!!");
+
+
+        for(int i = 0; i < currentPost.plan.size(); i++)
+        {
+            // String text plans
+            String[] splitPlan = currentPost.plan.get(i).split("&&");
+            String planMarker = splitPlan[0];
+            String planLabel = splitPlan[1];
+
+            String textWrapper = "";
+            if( i == 0)
+            {
+                textWrapper = "Start";
+            }else if(i == currentPost.plan.size() - 1)
+            {
+                textWrapper = "End";
+            }else
+            {
+                textWrapper = "Stop " + i;
+            }
+
+            LinearLayout markerLayout = createMarkerLayout(textWrapper);
+            LinearLayout labelLayout = createLabelLayout(planMarker.trim(), planLabel.trim());
+
+            locationMarkerWrapper.addView(markerLayout);
+            localtionLabelWrapper.addView(labelLayout);
+
+        }
+
+    }
+
+    private LinearLayout createMarkerLayout(String txt)
+    {
+        LinearLayout lo = new LinearLayout(getApplicationContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        lp.weight = 1;
+        lp.gravity = Gravity.CENTER;
+        lp.setMargins(0,10,0,10);
+
+        lo.setLayoutParams(lp);
+        lo.setPadding(10,10,10,10);
+        lo.setOrientation(LinearLayout.VERTICAL);
+
+        // IMAGE attach
+        ImageView imgView = new ImageView(getApplicationContext());
+
+        if(txt == "Start")
+            imgView.setImageResource(R.drawable.ic_location_marker_icon_green);
+        else if(txt == "End")
+            imgView.setImageResource(R.drawable.ic_location_marker_icon_orange);
+        else
+            imgView.setImageResource(R.drawable.ic_location_marker_icon);
+
+        // TEXT attach
+        TextView txtWrapper = new TextView(getApplicationContext());
+        LinearLayout.LayoutParams txtParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        txtWrapper.setLayoutParams(txtParams);
+        txtWrapper.setText(txt);
+
+        lo.addView(imgView);
+        lo.addView(txtWrapper);
+
+        return lo;
+
+    }
+
+    private LinearLayout createLabelLayout(String txt1, String txt2)
+    {
+        LinearLayout lo = new LinearLayout(getApplicationContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        lp.setMargins(0,10,0,10);
+        lo.setPadding(20,10,10,10);
+        lo.setOrientation(LinearLayout.VERTICAL);
+        lo.setLayoutParams(lp);
+
+
+        // TEXT attach1
+        TextView txtWrapper = new TextView(getApplicationContext());
+        LinearLayout.LayoutParams txtParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        txtWrapper.setLayoutParams(txtParams);
+        txtWrapper.setText(txt1);
+        txtWrapper.setTextSize(18.0f);
+        txtWrapper.setTypeface(null, Typeface.BOLD);
+
+        // TEXT attach2
+        TextView txtWrapper2 = new TextView(getApplicationContext());
+        LinearLayout.LayoutParams txtParams2 = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        txtWrapper2.setLayoutParams(txtParams2);
+        txtWrapper2.setText(txt2);
+
+        lo.addView(txtWrapper);
+        lo.addView(txtWrapper2);
+
+        return lo;
+    }
+
+
+    private void displayReviewSection()
+    {
+        reviewsWrapper = (LinearLayout)findViewById(R.id.reviewsWrapper);
+
+        for(int i = 0; i < 5; i++)
+        {
+            String username = "Fake name";
+            String date = "02/20/2019";
+            float rating = 3.0f;
+            String comment = "user comment";
+
+            LinearLayout bigWrapper = new LinearLayout(getApplicationContext());
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            bigWrapper.setPadding(10,10,10,10);
+            bigWrapper.setOrientation(LinearLayout.VERTICAL);
+
+            LinearLayout userAndRating = createUserAndRating(username, rating);
+            TextView dateView = new TextView(getApplicationContext());
+            dateView.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            dateView.setText(date);
+
+            TextView commentView = new TextView(getApplicationContext());
+            LinearLayout.LayoutParams commentParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            commentParams.setMargins(0,20,0,0);
+            commentView.setLayoutParams(commentParams);
+            commentView.setText(comment);
+
+            bigWrapper.addView(userAndRating);
+            bigWrapper.addView(dateView);
+            bigWrapper.addView(commentView);
+
+            reviewsWrapper.addView(bigWrapper);
+        }
+
+    }
+
+    private LinearLayout createUserAndRating(String name, float rating)
+    {
+        LinearLayout lo = new LinearLayout(getApplicationContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        lo.setOrientation(LinearLayout.HORIZONTAL);
+
+        TextView userView = new TextView(getApplicationContext());
+        LinearLayout.LayoutParams userlp = new LinearLayout.LayoutParams(
+                0,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        userlp.weight = 1;
+        userView.setLayoutParams(userlp);
+        userView.setText(name);
+        userView.setTextSize(18.0f);
+        userView.setTypeface(null, Typeface.BOLD);
+
+        RatingBar rb = new RatingBar(getApplicationContext(), null, android.R.attr.ratingBarStyleSmall);
+        LinearLayout.LayoutParams rblp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        rblp.gravity = Gravity.RIGHT;
+
+        rb.setNumStars(5);
+        rb.setStepSize(1);
+        rb.setRating(rating);
+        rb.setForegroundGravity(Gravity.RIGHT);
+        rb.setPadding(0,20,0,0);
+        rb.setLayoutParams(rblp);
+
+        lo.addView(userView);
+        lo.addView(rb);
+
+        return lo;
     }
 
     @Override
